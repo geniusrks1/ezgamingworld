dcec1ca5cf905155603980d42a38e9d6d8653a31
geniusrks1 <gmrohitkumar@gmail.com>
2023-10-31 20:36:36 +0530

    comments

--- src/extraPages/ToolTip/Tooltip.js
+++ src/extraPages/ToolTip/Tooltip.js
@@ -1,16 +1,29 @@
 // Tooltip.js
-import React from 'react';
-import styles from './Tooltip.module.css'; // Import the CSS module
+// This file defines a React component called Tooltip that displays a tooltip message on hover.
 
+// Import the React library to create React components
+import React from 'react';
+
+// Import the CSS module containing styles for the Tooltip component
+import styles from './Tooltip.module.css';
+
+// Define the Tooltip functional component that takes position, text, and children as props
 const Tooltip = ({ position, text, children }) => {
+  // Render the Tooltip component with provided position, text, and children props
   return (
+    // Container div to hold the tooltip and the child content
     <div className={styles.tooltipContainer}>
+      {/* Render the children components passed to the Tooltip */}
       {children}
+      
+      {/* Tooltip element with dynamic styles based on the provided position prop */}
       <div className={`${styles.tooltip} ${styles[position]}`}>
+        {/* Display the text content provided as a prop */}
         {text}
-        </div>
+      </div>
     </div>
   );
 };
 
+// Export the Tooltip component to be used in other parts of the application
 export default Tooltip;
--- src/extraPages/ToolTip/TooltipHome.js
+++ src/extraPages/ToolTip/TooltipHome.js
@@ -1,14 +1,19 @@
+// Import necessary libraries and components from React and external files
 import React, { useEffect, useState } from 'react';
-import Tooltip from './Tooltip';
-import { GrTooltip } from 'react-icons/gr';
+import Tooltip from './Tooltip'; // Import the custom Tooltip component
+import { GrTooltip } from 'react-icons/gr'; // Import an icon component from the react-icons library
 
+// Define the TooltipHome functional component that demonstrates the usage of the Tooltip component
 const TooltipHome = () => {
-  const [selectedPosition, setSelectedPosition] = useState('top'); // Default position is 'top'
+  // State variable to manage the selected position of the tooltip, default is 'top'
+  const [selectedPosition, setSelectedPosition] = useState('top');
 
+  // Event handler function to update the selected tooltip position based on radio button selection
   const handleRadioChange = (position) => {
     setSelectedPosition(position);
   };
 
+  // Styles for the container, label, radio buttons, and heading elements
   const containerStyle = {
     backgroundColor: '#f0f0f0',
     height: '100vh',
@@ -33,28 +38,36 @@ const TooltipHome = () => {
     textDecoration: 'underline dotted',
   };
 
-  useEffect(()=>{
-    document.title="Tooltip";
-    document.querySelector('link[rel="icon"]').setAttribute('href', GrTooltip());
-},[])
+  // useEffect hook to set the document title and favicon when the component mounts
+  useEffect(() => {
+    document.title = "Tooltip"; // Set the document title
+    document.querySelector('link[rel="icon"]').setAttribute('href', GrTooltip()); // Set the favicon using the imported icon component
+  }, []); // Empty dependency array ensures that the effect runs only once after the initial render
 
+  // Render the component UI with radio buttons and the Tooltip component
   return (
     <div style={containerStyle}>
+      {/* Radio buttons to select tooltip position */}
       <div style={labelStyle}>
         <label>
           Select Tooltip Position:
           <br />
+          {/* Radio buttons for top, bottom, right, and left positions */}
           <input type="radio" name="position" value="top" checked={selectedPosition === 'top'} onChange={() => handleRadioChange('top')} style={radioStyle} /> Top
           <input type="radio" name="position" value="bottom" checked={selectedPosition === 'bottom'} onChange={() => handleRadioChange('bottom')} style={radioStyle} /> Bottom
           <input type="radio" name="position" value="right" checked={selectedPosition === 'right'} onChange={() => handleRadioChange('right')} style={radioStyle} /> Right
           <input type="radio" name="position" value="left" checked={selectedPosition === 'left'} onChange={() => handleRadioChange('left')} style={radioStyle} /> Left
         </label>
       </div>
-      <Tooltip position={selectedPosition} text="Thanks for hovering! I'm toolkit">
+      
+      {/* Tooltip component with selected position and text */}
+      <Tooltip position={selectedPosition} text="Thanks for hovering! I'm tooltip">
+        {/* Heading element with underline, indicating where to hover for the tooltip */}
         <h4 style={headingStyle}>Hover Over Me! </h4>
       </Tooltip>
     </div>
   );
 };
 
+// Export the TooltipHome component to be used in other parts of the application
 export default TooltipHome;
